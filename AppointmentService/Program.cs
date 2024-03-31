635b94e6d751df9c605e94c77fbf1a7d2abf20f5
Merve Şahinoğlu <artemiswolfs@gmail.com>
2024-03-31 16:03:07 +0300

    migrasyon için

--- AppointmentService/Program.cs
+++ AppointmentService/Program.cs
@@ -49,6 +49,12 @@ try
 
     var app = builder.Build();
 
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppointmentServiceContext>();
+        db.Database.Migrate();
+    }
+
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
